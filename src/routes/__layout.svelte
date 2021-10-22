80c6ba818ed7e742fb2fd7528f1881e8d57e5440
Danil Golovachev <danithecake@gmail.com>
2021-10-23 01:52:13 +0300

    feat(routes): resolve parsed players data in root layout component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,26 @@
+<script lang="ts" context="module">
+  import type { Load } from '@sveltejs/kit'
+
+  export const load: Load = async ({ fetch }) => {
+    try {
+      return { props: { players: await (await fetch('/players')).json() } }
+    } catch (err) {
+      return { props: { players: {} } }
+    }
+  }
+</script>
+
+<script lang="ts">
+  import type { IPlayer } from 'common/interfaces'
+  import { setContext } from 'svelte'
+
+  export let players: Record<IPlayer['name'], IPlayer>
+
+  setContext('players', players)
+</script>
+
+<slot />
+
 <style global lang="postcss">
   @tailwind components;
   @tailwind utilities;
